64bbf4abb004a9df7f68ad1a92b6361669468d84
juliaallencar16 <juliaallencar16@gmail.com>
2026-02-18 12:06:32 -0300

    primeiro commit

---
+++ Pincipal.java
@@ -0,0 +1,13 @@
+package aulaJPA.aula01;
+
+public class Pincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   // esse código printa o texto na tela 
+		System.out.println("julia nunes");
+	
+	}
+	
+
+}
